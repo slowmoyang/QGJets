59609aae1482b8c500d9c36294c90b42f4024074
slowmoyang <slowmoyang@gmail.com>
2018-01-13 16:25:20 +0900

    Add large filter model

--- SJ-Keras/keras4jet/models/image/__init__.py
+++ SJ-Keras/keras4jet/models/image/__init__.py
@@ -1,5 +1,13 @@
 from __future__ import absolute_import
 
 from . import resnet
+from . import capsnet
+
 from . import simple_cnn
 from . import asym
+from . import large_filter
+from . import conv_stems
+
+# finebin
+from . import finebin_bottleneck
+from . import finebin_largefilter
