3d23020d3bf9f5e13a55866bac40823bc69bba1b
slowmoyang <slowmoyang@gmail.com>
2018-02-03 16:26:25 +0900

    Add pdgid.cc

---
+++ SJ-JetImage/pdgid.cc
@@ -0,0 +1,13 @@
+enum class PdgId: int {
+    kElectron = 11,
+    kPositron = -11,
+    kAntiElectron = -11,
+    kMuon = 13,
+    kAntiMuon = -13,
+    kPhoton = 22,
+};
+
+inline bool operator== (int pid0, PdgId pid1)
+{
+    return pid0 == static_cast<int>(pid1);
+}
