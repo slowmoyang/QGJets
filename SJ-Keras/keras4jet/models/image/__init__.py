6e968d59a746e4f4c1ba69a62a3a3311f1baa01e
slowmoyang <slowmoyang@gmail.com>
2018-02-05 20:24:00 +0900

    Commit keras4jet/models/image/__init__.py for git pull

--- SJ-Keras/keras4jet/models/image/__init__.py
+++ SJ-Keras/keras4jet/models/image/__init__.py
@@ -1,15 +1,7 @@
 from __future__ import absolute_import
 
 
-from . import simple_cnn
-from . import asym
 from . import large_filter
-from . import conv_stems
-
-# finebin
-from . import finebin_bottleneck
-from . import finebin_largefilter
-
 
 # Model Comparison
 from . import resnet
