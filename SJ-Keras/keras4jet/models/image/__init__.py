dfb0a1f269ce0bc50fb1931488eefeed567697ef
slowmoyang <slowmoyang@gmail.com>
2018-02-05 20:20:29 +0900

    Update keras4jet/models/image

--- SJ-Keras/keras4jet/models/image/__init__.py
+++ SJ-Keras/keras4jet/models/image/__init__.py
@@ -1,17 +1,11 @@
 from __future__ import absolute_import
 
 
-from . import simple_cnn
-from . import asym
 from . import large_filter
-from . import conv_stems
-
-# finebin
-from . import finebin_bottleneck
-from . import finebin_largefilter
-
+from . import large_filter_v2
 
 # Model Comparison
 from . import resnet
 from . import densenet
 from . import capsnet
+from . import inception_resnet_v2
