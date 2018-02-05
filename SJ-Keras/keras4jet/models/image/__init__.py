6747448c330ef12c283287276599e074b7966021
slowmoyang <slowmoyang@gmail.com>
2018-02-05 20:28:25 +0900

    Update __init__.py

---
+++ SJ-Keras/keras4jet/models/image/__init__.py
@@ -0,0 +1,11 @@
+from __future__ import absolute_import
+
+
+from . import large_filter
+from . import large_filter_v2
+
+# Model Comparison
+from . import resnet
+from . import densenet
+from . import capsnet
+from . import inception_resnet_v2
