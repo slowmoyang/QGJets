7db75c8dc3b56580971cd3c15dc8c5dd9a0a33e3
slowmoyang <slowmoyang@gmail.com>
2018-02-13 00:24:52 +0900

    Add scripts for Xception model

--- SJ-Keras/keras4jet/models/image/__init__.py
+++ SJ-Keras/keras4jet/models/image/__init__.py
@@ -4,8 +4,7 @@ from __future__ import absolute_import
 from . import large_filter
 from . import large_filter_v2
 
-# Model Comparison
-from . import resnet
 from . import densenet
 from . import capsnet
 from . import inception_resnet_v2
+from . import xception
