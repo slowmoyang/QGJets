b60f84c6a1a4e970f38130481e005492b2e3a9df
slowmoyang <slowmoyang@gmail.com>
2018-04-13 11:36:08 +0900

    Change the inner structure of Forest

--- SJ-AfterAnalyser/src/Forest.cc
+++ SJ-AfterAnalyser/src/Forest.cc
@@ -12,73 +12,71 @@
 #include <numeric> // std::acuumulate
 #include <unordered_map>
 #include <iostream>
+#include <memory>
+#include <cstdlib> // std::rand, std::abort
 
 
 Forest::Forest(std::vector<TString> paths, TString tree_name) {
-  paths_ = paths;
   tree_name_ = tree_name;
 
-  for(auto path : paths_) {
+  Int_t idx = 0;
+  num_total_entries_ = 0;
+  for(auto path : paths) {
+
     TFile* root_file = TFile::Open(path, "READ");
     TTree* tree = dynamic_cast<TTree*>(root_file->Get(tree_name_));
     Int_t num_entries = tree->GetEntries();
 
-    files_.push_back(root_file);
-    trees_.push_back(tree);
-    num_entries_.push_back(num_entries);
-  }
+    num_total_entries_ += num_entries;
 
-  tree_candidates_.assign(trees_.begin(), trees_.end());
-  num_candidates_ = tree_candidates_.size();
-  entry_candidates_.resize(num_candidates_);
-  std::fill(entry_candidates_.begin(), entry_candidates_.end(), 0);
+    candidates_[idx] = BigTree();
+    candidates_[idx].path        = path;
+    candidates_[idx].file        = root_file;
+    candidates_[idx].tree        = tree;
+    candidates_[idx].entry       = 0;
+    candidates_[idx].num_entries = num_entries;
+
+    alives_.push_back(idx);
+
+    idx++;
+  }
 }
 
 
-Forest::~Forest() {}
+void Forest::GetEntry() {
+  // random choice
+  Int_t alives_idx = std::rand() % alives_.size();
+  Int_t idx = alives_[alives_idx];
 
+  candidates_[idx].tree->GetEntry(candidates_[idx].entry);
+  candidates_[idx].entry++;
 
-Int_t Forest::GetEntry() {
-  Int_t tree_idx = gRandom->Uniform(num_candidates_);
-
-  Int_t get_entry_out = tree_candidates_[tree_idx]->GetEntry(entry_candidates_[tree_idx]); 
-  entry_candidates_[tree_idx]++;
-
-  if(entry_candidates_[tree_idx] == num_entries_[tree_idx]) {
-    tree_candidates_.erase(tree_candidates_.begin() + tree_idx);
-    entry_candidates_.erase(entry_candidates_.begin() + tree_idx);
-
-    num_candidates_ = tree_candidates_.size(); 
+  if(candidates_[idx].entry == candidates_[idx].num_entries) {
+    std::cout << candidates_[idx].path << "'s tree is exhausted" << std::endl;
+    alives_.erase(alives_.begin() + alives_idx);
   }
-
-  return 0;
 }
 
 
 TTree* Forest::CloneTree() {
-  return trees_[0]->CloneTree(0);
+  return candidates_[0].tree->CloneTree(0);
 }
 
 void Forest::CopyAddress(TTree* tree) {
-  for(TTree* each : trees_) {
-    tree->CopyAddresses(each);
-  }
+  for(auto each : candidates_)
+    tree->CopyAddresses(each.second.tree);
 }
 
 
 Bool_t Forest::HasEntry() {
-    Bool_t has_entry = static_cast<Bool_t>(num_candidates_);
-    return has_entry;
+  Bool_t has_entry;
+  if (num_alive_ > 0)       has_entry = true;
+  else if (num_alive_ == 0) has_entry = false;
+  else                      std::abort();
+  return has_entry;
 }
 
 
-Int_t Forest::GetEntries() {
-  return std::accumulate(num_entries_.begin(), num_entries_.end(), 0);
-}
+Int_t Forest::GetEntries() {return num_total_entries_;}
 
 
-void Forest::Close() {
-  for(auto each : files_) {
-    each->Close();
-  }
-}
--- SJ-AfterAnalyser/src/Forest.h
+++ SJ-AfterAnalyser/src/Forest.h
@@ -6,32 +6,37 @@
 #include "TString.h"
 
 #include <vector>
+#include <map>
 // #include <cstdlib>
 #include <numeric>
 #include <unordered_map>
 
+struct BigTree {
+  TString path;
+  TFile*   file;
+  TTree*   tree;
+  Int_t    entry;
+  Int_t    num_entries;
+};
+
+
 class Forest {
 public:
   Forest(std::vector<TString> paths, TString tree_name);
-  ~Forest();
 
   TTree* CloneTree();
   void CopyAddress(TTree* tree);
-  Int_t GetEntry();
+  void GetEntry();
   Int_t GetEntries();
   Bool_t HasEntry();
-  void Close();
 
 private:
-  std::vector<TString> paths_;
   TString tree_name_;
-  std::vector<TFile*>  files_;
-  std::vector<TTree*>  trees_;
-  std::vector<Int_t>   num_entries_;
+  std::map<Int_t, BigTree> candidates_;
+  std::vector<Int_t> alives_;
+  Int_t num_alive_;
 
-  std::vector<TTree*> tree_candidates_;
-  std::vector<Int_t> entry_candidates_;
-  Int_t num_candidates_;
+  Int_t num_total_entries_;
 };
 
 #endif
