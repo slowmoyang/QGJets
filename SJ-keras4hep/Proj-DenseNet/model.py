4ad6a53d1f5cec75e52a057479d5aa1457146911
slowmoyang <slowmoyang@gmail.com>
2018-11-28 19:37:18 +0900

    Add a project: DenseNet + JetImage

---
+++ SJ-keras4hep/Proj-DenseNet/model.py
@@ -0,0 +1,126 @@
+import tensorflow.keras
+import tensorflow.keras.backend as K
+from tensorflow.keras import layers
+from tensorflow.keras import models
+from tensorflow.keras.layers import Input
+
+def dense_block(x, blocks, name):
+    """A dense block.
+    # Arguments
+        x: input tensor.
+        blocks: integer, the number of building blocks.
+        name: string, block label.
+    # Returns
+        output tensor for the block.
+    """
+    for i in range(blocks):
+        x = conv_block(x, 32, name=name + '_block' + str(i + 1))
+    return x
+
+
+def transition_block(x, reduction, name):
+    """A transition block.
+    # Arguments
+        x: input tensor.
+        reduction: float, compression rate at transition layers.
+        name: string, block label.
+    # Returns
+        output tensor for the block.
+    """
+    bn_axis = 3 if K.image_data_format() == 'channels_last' else 1
+    x = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
+                                  name=name + '_bn')(x)
+    x = layers.Activation('relu', name=name + '_relu')(x)
+    x = layers.Conv2D(int(K.int_shape(x)[bn_axis] * reduction), 1,
+                      use_bias=False,
+                      name=name + '_conv')(x)
+    x = layers.AveragePooling2D(2, strides=2, name=name + '_pool')(x)
+    return x
+
+
+def conv_block(x, growth_rate, name):
+    """A building block for a dense block.
+    # Arguments
+        x: input tensor.
+        growth_rate: float, growth rate at dense layers.
+        name: string, block label.
+    # Returns
+        Output tensor for the block.
+    """
+    bn_axis = 3 if K.image_data_format() == 'channels_last' else 1
+    x1 = layers.BatchNormalization(axis=bn_axis,
+                                   epsilon=1.001e-5,
+                                   name=name + '_0_bn')(x)
+    x1 = layers.Activation('relu', name=name + '_0_relu')(x1)
+    x1 = layers.Conv2D(4 * growth_rate, 1,
+                       use_bias=False,
+                       name=name + '_1_conv')(x1)
+    x1 = layers.BatchNormalization(axis=bn_axis, epsilon=1.001e-5,
+                                   name=name + '_1_bn')(x1)
+    x1 = layers.Activation('relu', name=name + '_1_relu')(x1)
+    x1 = layers.Conv2D(growth_rate, 3,
+                       padding='same',
+                       use_bias=False,
+                       name=name + '_2_conv')(x1)
+    x = layers.Concatenate(axis=bn_axis, name=name + '_concat')([x, x1])
+    return x
+
+
+def build_a_model(input_shape=None,
+             blocks=[6, 12, 24, 16]):
+
+    img_input = layers.Input(shape=input_shape)
+    bn_axis = 3 if K.image_data_format() == 'channels_last' else 1
+
+    x = layers.ZeroPadding2D(padding=((3, 3), (3, 3)))(img_input)
+    x = layers.Conv2D(64, 7, strides=2, use_bias=False, name='conv1/conv')(x)
+    x = layers.BatchNormalization(
+        axis=bn_axis, epsilon=1.001e-5, name='conv1/bn')(x)
+    x = layers.Activation('relu', name='conv1/relu')(x)
+    x = layers.ZeroPadding2D(padding=((1, 1), (1, 1)))(x)
+    x = layers.MaxPooling2D(3, strides=2, name='pool1')(x)
+
+    x = dense_block(x, blocks[0], name='conv2')
+    x = transition_block(x, 0.5, name='pool2')
+    x = dense_block(x, blocks[1], name='conv3')
+    x = transition_block(x, 0.5, name='pool3')
+    x = dense_block(x, blocks[2], name='conv4')
+    x = transition_block(x, 0.5, name='pool4')
+    x = dense_block(x, blocks[3], name='conv5')
+
+    x = layers.BatchNormalization(
+        axis=bn_axis, epsilon=1.001e-5, name='bn')(x)
+    x = layers.Activation('relu', name='relu')(x)
+
+    x = layers.GlobalAveragePooling2D(name='avg_pool')(x)
+    x = layers.Dense(2, activation='softmax', name='fc1000')(x)
+
+    inputs = img_input
+
+    # Create model.
+    model = models.Model(img_input, x, name='densenet')
+
+
+
+    return model
+
+
+
+def _test():
+    from dataset import C10Set
+    from keras4hep.data import DataIterator
+    path = "/store/slowmoyang/QGJets/dijet_100_110/dijet_100_110_test.root"
+    dset = C10Set(path)
+    data_iter = DataIterator(dset, batch_size=128)
+
+    batch = data_iter.next()
+    x_shape = data_iter.get_shape("x", batch_shape=False)
+
+    model = build_a_model(x_shape)
+
+    logits = model.predict_on_batch(batch.x)
+    print("logits: {}".format(logits.shape))
+
+
+if __name__ == "__main__":
+    _test()
