f26224ba529ef6ee10cab56b2b1357d51568408c
slowmoyang <slowmoyang@gmail.com>
2019-01-03 11:32:52 +0900

    Move functionalities of the follw1-AnalyseJets, SJ-AfterAnalyser and SJ-JetImage to the 'delphys' project. Refer to https://github.com/slowmoyang/delphys/blob/master/analysis/bin/QGJetsAnalyser.cc

--- SJ-AfterAnalyser/SplitAndShuffle/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-IN_PATH=$1
-
-cd $CMSSW_RELEASE_BASE
-eval `scram runtime -sh`
-cd -
-
-./split_and_shuffle ${IN_PATH}
--- SJ-AfterAnalyser/autoMS.sh
+++
@@ -1,5 +0,0 @@
-EVENT_TYPE=$1
-CMD="cmsenv && ./merge_and_shuffle ${EVENT_TYPE}"
-NAME="MS_${EVENT_TYPE}" # session name
-tmux new -s ${NAME} -d
-tmux send-keys -t ${NAME} "${CMD}" C-m
